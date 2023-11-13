63657144d4c330020413b0770e72688907f29b7c
Jihong Min <jihong@csap.snu.ac.kr>
2023-11-13 11:20:18 +0900

    Check X86LINUX_SUPPRESS_LOG first to not log anything

--- src/x86linuxextra.c
+++ src/x86linuxextra.c
@@ -1,17 +1,17 @@
 #include "x86linux/helper.h"
 
 static __attribute__((constructor(101))) void _x86linuxextra_init(void) {
+  /* Suppress logging first if required. */
+  const char *env_suppress_log = getenv("X86LINUX_SUPPRESS_LOG");
+  if (env_suppress_log) {
+    /* Do not log here! */
+    _suppress_log = 1;
+  }
+
   log_info("libx86linuxextra 0.1.0 - Jihong Min (hurryman2212@gmail.com)");
 #ifndef NDEBUG
   log_info("You are using `Debug` build.");
 #else
   log_info("You are using `Release` build.");
 #endif
-
-  const char *env_suppress_log = getenv("X86LINUX_SUPPRESS_LOG");
-  if (env_suppress_log) {
-    log_warn("X86LINUX_SUPPRESS_LOG=%s -> All logging will be suppressed!",
-             env_suppress_log);
-    _suppress_log = 1;
-  }
 }
